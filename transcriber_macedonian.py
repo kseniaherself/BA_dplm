820f050617ae24dabd7cec496d03eeeed5a0c154
Ksenia Romanova <zeviya25@gmail.com>
2019-05-06 20:24:14 +0300

    macedonian transcribed
    program and a list of transcribed macedonian words.

---
+++ transcriber_macedonian.py
@@ -0,0 +1,118 @@
+import re
+import operator
+import time
+start_time = time.time()
+
+# чтение строк из файла, возвращает строки
+def F_get_lines(f_name):
+    f = open(f_name, 'r')
+    my_lines = f.readlines()
+    #print(my_lines)
+    f.close()
+    return my_lines
+
+# запись в файл
+def F_write_in_file(data , f_name):
+    f = open(f_name, 'w')
+    f.write(data)
+    #print(data)
+    f.close()
+
+# разбиение на символы: на входе слово, на выходе оно в разделённом виде
+def F_lettering(word):
+    lettering = ''
+
+    for element in word:
+        lettering = lettering + '-' + element
+    lettering = lettering.replace('-', '', 1)
+
+    return lettering
+
+# транскриптор: на выходе слово в МФА +слоговые сонорные
+def F_ipa_transcriber(word):
+
+    cyrillic_s = ['а', 'б', 'в', 'г', 'д', 'ѓ', 'е', 'ж', 'з', 'ѕ', 'и', 'ј', 'к', 'л', 'љ', 'м', 'н', 'њ',
+                  'о', 'п', 'р', 'с', 'т', 'ќ', 'у', 'ф', 'х', 'ц', 'ч', 'џ', 'ш', 'ѐ', 'ѝ']
+
+    ipa_s = ['a', 'b', 'v', 'ɡ', 'd', 'ɟ', 'e', 'ʒ', 'z', 'dz', 'i', 'j', 'k', 'l', 'lj', 'm', 'n', 'nj',
+             'o', 'p', 'r', 's', 't', 'c', 'u', 'f', 'x', 'ts', 'tʃ', 'dʒ','ʃ', 'è', 'ì']
+
+    for element in word:
+        for i in range(0, 33):
+            if cyrillic_s[i] == element:
+                word = word.replace(cyrillic_s[i], ipa_s[i])
+
+# добавление слоговых сонорных: r, l, m, n
+    ipa_consonants = ['b', 'v', 'ɡ', 'd', 'ɟ', 'ʒ', 'z', 'dz', 'k', 'l', 'lj', 'm', 'n', 'nj',
+                      'p', 'r', 's', 't', 'c', 'f', 'x', 'ts', 'tʃ', 'dʒ', 'ʃ'] # add ['R'], минус ['j',]
+
+    for j in range(0, 25):
+        environment_in_left = '^r-' + str(ipa_consonants[j])
+        environment_out_left = 'R-' + str(ipa_consonants[j])
+        word = re.sub(environment_in_left, environment_out_left, word)
+
+        environment_in_right_r = str(ipa_consonants[j]) + '-r$'
+        environment_out_right_r = str(ipa_consonants[j]) + '-R'
+        word = re.sub(environment_in_right_r, environment_out_right_r, word)
+
+        for k in range (0, 25):
+            environment_in = str(ipa_consonants[j]) + '-r-' + str(ipa_consonants[k])
+            environment_out = str(ipa_consonants[j]) + '-R-' + str(ipa_consonants[k])
+            word = word.replace(environment_in, environment_out)
+
+    for l in range(0, 25):
+        environment_in_right_l = str(ipa_consonants[l]) + '-l$'
+        environment_out_right_l = str(ipa_consonants[l]) + '-L'
+        word = re.sub(environment_in_right_l, environment_out_right_l, word)
+
+        environment_in_right_n = str(ipa_consonants[l]) + '-n$'
+        environment_out_right_n = str(ipa_consonants[l]) + '-N'
+        word = re.sub(environment_in_right_n, environment_out_right_n, word)
+
+    return word
+
+# основная функция
+def m_1():
+    f_name = 'words_macedonian.txt'
+    f_name_2 = 'ipa_words_macedonian.txt'
+
+    print('im in!')
+
+    my_lines = F_get_lines(f_name)
+    asd = my_lines[1:]             # РАБОЧАЯ ВЕРСИЯ ДЛЯ ВСЕХ СЛОВ
+    #asd = my_lines[1:7]             # ТЕСТОВАЯ ЧАСТЬ
+
+    ipa_words = 'ipa_macedonian'
+
+    for line in asd:
+        entry = line.strip()
+
+        #the_word = ''
+
+        for element in entry:
+            #print(element)
+
+            if element == '-':
+                entry = entry.replace('-', '_')
+
+            entry = entry.replace('!', '')              # пусть будет
+
+            lettering = F_lettering(entry)
+
+            lower_lettering = str.lower(lettering)  # на случай имён
+            ipa_word = F_ipa_transcriber(lower_lettering)
+
+            #the_word = the_word + ipa_word
+
+        #print(the_word)
+        ipa_words = ipa_words + '\n' + ipa_word
+
+    #print(ipa_words)
+
+
+    F_write_in_file(ipa_words, f_name_2)
+
+m_1()
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
