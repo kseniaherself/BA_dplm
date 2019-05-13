94c2ec1c7c019f76ba3d93b1d6bd1a1fb55b2768
Ksenia Romanova <zeviya25@gmail.com>
2019-05-13 23:58:00 +0300

    upd

--- transpose_russian.py
+++ transpose_russian.py
@@ -48,18 +48,75 @@ def F_transposer(word):
     ipa_mutable_sonorant = ['N', 'N', 'L', 'R']
 
     hard_voc = ['а', 'о', 'у', 'э', 'ы']
-    ips_hard_voc = ['a', 'o', 'u', 'ɛ', 'y']
+    ipa_hard_voc = ['A', 'Ɔ', 'U', 'Ɛ', 'Y']
 
     jot_voc = ['я', 'ё', 'ю', 'е']
-    ipa_jot_voc = ['a', 'o', 'u', 'e']
+    ipa_jot_voc = ['A', 'Ɔ', 'U', 'E']
 
     soft_voc = ['и']
-    ipa_soft_voc = ['i']
+    ipa_soft_voc = ['I']
+
+    jot = 'й'
+    ipa_jot = 'J'
 
     er = ['ъ']
     erj = ['ь']
 
-    # use replace не sub !!!!! 
+    # печатает слова с оригинальным '-'
+    #if '_' in word:
+    #    print(word)
+
+    # печатает слова с ъ/ь после hard immutable
+    #for a in range (1, 3):
+        #if (hard_immut[a] + '-ь') in word:
+            #print(word)
+
+    # печатает слова с й перед йотированным гласным
+    #for a in range (1, len(jot_voc)):
+    #    if ('-й-' + jot_voc[a]) in word:
+    #        print(word)
+
+    # use replace не sub !!!!!
+    for element in word:
+
+        for m in range(0, len(hard_voc)):
+            if hard_voc[m] == element:
+                word = word.replace(hard_voc[m], ipa_hard_voc[m])
+                print(word)
+
+        for i in range(0, len(hard_immut)):
+            if hard_immut[i] == element:
+                word = word.replace(hard_immut[i], ipa_hard_immut[i])
+
+        for j in range(0, len(soft_immut)):
+            if soft_immut[j] == element:
+                word = word.replace(soft_immut[j], ipa_soft_immut[j])
+
+    word = word.replace(jot, ipa_jot)
+
+    for k in range(1, len(jot_voc)):
+        word = word.replace(('O-' + jot_voc[k]), ('O-' + ipa_hard_voc[k]))
+
+    for elem in word:
+        for l in range(1, len(mutable_obstr)):
+            if mutable_obstr[l] == elem:
+                word = word.replace(mutable_obstr[l], ipa_mutable_obstr[l])
+
+    for m in range(1, len(jot_voc)):
+        word = word.replace(('O-' + jot_voc[m]), ('O-' + ipa_jot_voc[m]))
+
+
+
+
+    #if '' in word:
+    #    word = word.replace('', '')
+
+    #for a in range(1, len(jot_voc)):
+    #    if ('O-' + jot_voc[a]) in word:
+    #        print(word)
+
+
+    #print(word)
 
 
 def M_1():
@@ -67,14 +124,16 @@ def M_1():
     f_name = 'lexemes_russian.tsv'
     #f2_name = 'ipa_' + f_name
     #df = pd.read_csv(f_name, sep='\t', usecols=['lexemes'])
-    df = pd.read_csv(f_name, sep='\t', usecols=['lexemes'], nrows = 15)
+    df = pd.read_csv(f_name, sep='\t', usecols=['lexemes'], nrows = 35)
 
     for elem in df['lexemes']:
-        elem_low = elem.lower()
+        elem_1 = elem.replace('-', '_')
+        elem_low = elem_1.lower()
         #print(elem_low)
         elem_let = F_lettering(elem_low)
-        print(elem_let)
+        #print(elem_let)
         elem_ipa = 'xz'
+        F_transposer(elem_let)
 
 
 
